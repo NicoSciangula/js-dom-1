f5643131bcb01e13aa6a09f301f9c663d53984f4
NicoSciangula <nico.sciangula99@gmail.com>
2025-12-18 00:07:45 +0100

    aggiunta condizione if bonus

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const btn = document.getElementById("light-on/off");
 const lightOn = document.querySelector(".light-on");
 const lightOff = document.querySelector(".lamp");
+let contatore = 0;
 
 btn.addEventListener("click", () => {
   lightOn.classList.toggle("toggle-lamp");
   lightOff.classList.toggle("light-off");
-  if (lightOn.classList.contains("toggle-lamp")) {
+  if (contatore === 10) {
+    btn.textContent = "Classe 11 domina!";
+    contatore = 0;
+  } else if (lightOn.classList.contains("toggle-lamp")) {
     textOff();
+    contatore++;
   } else {
     textOn();
+    contatore++;
   }
 });
 
