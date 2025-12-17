e705d70949e77d2c4a5b7340cbd8b3e7781aaac0
NicoSciangula <nico.sciangula99@gmail.com>
2025-12-17 23:05:40 +0100

    aggiunto evento su btn

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const btn = document.getElementById("light-on/off");
-const lightOn = document.querySelector("div.light-on");
-const lightOff = document.querySelector("div.lamp");
+const lightOn = document.querySelector(".light-on");
+const lightOff = document.querySelector(".lamp");
+
+btn.addEventListener("click", () => {
+  lightOn.classList.add("toggle-lamp");
+  lightOff.classList.add("light-off");
+});
+
